2660481c6e3385320e1686bd06d168a80b161154
subodhgautam <subodhgtm08@gmail.com>
2026-04-09 09:12:49 +0545

    Layed Basic Layout
    Just for the commit.
    Only basic layout completed.
    Styling and logic remains

---
+++ script.js
@@ -0,0 +1,10 @@
+const balance = document.getElementById("balance");
+const income_val = document.getElementById("income_val");
+const expense_val = document.getElementById("expense_val");
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const add_btn = document.getElementById("add_btn");
+
+
+
+// hackatime
